7d8cf1b5b766409734ffd91fc3151fce8e97c662
NataliaGatarek <nova.nathalia@gmail.com>
2021-05-16 21:34:57 +0200

    2nd fetch

--- src/Components/More.js
+++ src/Components/More.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from "./Cards.js"
 import {
   BrowserRouter as Router,
@@ -9,11 +9,31 @@ import {
 } from "react-router-dom";
 
 function More(props) {
-    let { webTitle } = useParams();
+    const { webTitle } = useParams();
+    const [more, setMore] = useState([]);
+
+    useEffect(() => {
+        const fetchMore = () => {
+            fetch(`http://content.guardianapis.com/tags=${webTitle}?api-key=e59054b6-4cd2-4e33-8805-7fc6efe12221`)
+                .then((response) =>
+                    response.json()
+                )
+                .then((data) => {
+                    setMore(data.response.results);
+                });
+        }
+        fetchMore();
+    }, []);
+
     return (
-        <div className="flex-cards"><Cards>
-            <p>{webTitle}</p></Cards>
+        <div>{
+             more.map((more) => {
+                    return (
+                       <div className="flex-cards" key={more.id}><Cards>
+                       <p>{webTitle}</p></Cards>
+                       </div> 
+                      )})
+        }
         </div>
-    )
-}
+)}
 export default More;
