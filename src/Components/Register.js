42baf24f450bb3035f53dfc8c6125909832a8f66
NataliaGatarek <nova.nathalia@gmail.com>
2021-05-25 15:45:18 +0200

    LogIn+Register

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import "./Cards.css";
 import { LogoContext } from "../context/LogoContext.js";
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Register() {
